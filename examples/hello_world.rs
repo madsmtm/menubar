e102b8f2dd99302e1544419a262625ff467d1f03
Mads Marquart <mads@marquart.dk>
2021-01-26 14:48:08 +0100

    Winit setup and MacOS tests

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,134 +1,109 @@
 #![windows_subsystem = "windows"]
 
-#[cfg(windows)]
-use std::os::windows::prelude::*;
-#[cfg(windows)]
-use std::{ffi::OsStr, mem, ptr};
+#[cfg(target_os = "macos")]
+use objc::msg_send;
 
-#[cfg(windows)]
-use winapi::shared::{minwindef::UINT, ntdef::LPCWSTR};
-#[cfg(windows)]
-use winapi::um::{
-    libloaderapi,
-    winuser::{
-        CreateWindowExW, DefWindowProcW, DispatchMessageW, GetMessageW, MessageBoxW,
-        RegisterClassExW, ShowWindow, TranslateMessage, UpdateWindow, CS_HREDRAW, CS_VREDRAW,
-        CW_USEDEFAULT, LPMSG, SW_SHOW, WNDCLASSEXW, WS_OVERLAPPEDWINDOW,
-    },
+use raw_window_handle::{HasRawWindowHandle, RawWindowHandle};
+use std::error::Error;
+#[cfg(target_os = "macos")]
+use winit::platform::macos::WindowBuilderExtMacOS;
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
 };
 
-// #[cfg(windows)]
-// unsafe fn wnd_proc(h_wnd: HWND, message: UINT, w_param: WPARAM, l_param: LPARAM) -> LRESULT {
-//     dbg!(h_wnd, message, w_param, l_param);
-// }
-
-#[cfg(windows)]
-unsafe fn windows_window() {
-    let class_name: Vec<_> = OsStr::new("DesktopApp")
-        .encode_wide()
-        .chain(Some(0).into_iter())
-        .collect();
-
-    let class = WNDCLASSEXW {
-        cbSize: mem::size_of::<WNDCLASSEXW>() as UINT,
-        style: CS_HREDRAW | CS_VREDRAW,
-        lpfnWndProc: Some(DefWindowProcW),
-        cbClsExtra: 0,
-        cbWndExtra: 0,
-        hInstance: libloaderapi::GetModuleHandleW(ptr::null()),
-        hIcon: ptr::null_mut(),
-        hCursor: ptr::null_mut(),
-        hbrBackground: ptr::null_mut(),
-        lpszMenuName: ptr::null_mut(),
-        lpszClassName: class_name.as_ptr(),
-        hIconSm: ptr::null_mut(),
-    };
-
-    if RegisterClassExW(&class) == 0 {
-        MessageBoxW(
-            ptr::null_mut(),
-            OsStr::new("Call to RegisterClassEx failed!")
-                .encode_wide()
-                .chain(Some(0).into_iter())
-                .collect::<Vec<_>>()
-                .as_ptr(),
-            OsStr::new("Windows Desktop Guided Tour")
-                .encode_wide()
-                .chain(Some(0).into_iter())
-                .collect::<Vec<_>>()
-                .as_ptr(),
-            0,
-        );
-        return;
-    }
-
-    let title = OsStr::new("Windows Desktop Guided Tour Application")
-        .encode_wide()
-        .chain(Some(0).into_iter())
-        .collect::<Vec<_>>();
-
-    let handle = CreateWindowExW(
-        0,
-        class_name.as_ptr(),
-        title.as_ptr() as LPCWSTR,
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        ptr::null_mut(),
-        ptr::null_mut(),
-        libloaderapi::GetModuleHandleW(ptr::null()),
-        ptr::null_mut(),
-    );
-
-    if handle.is_null() {
-        MessageBoxW(
-            ptr::null_mut(),
-            OsStr::new("Call to CreateWindow failed!")
-                .encode_wide()
-                .chain(Some(0).into_iter())
-                .collect::<Vec<_>>()
-                .as_ptr(),
-            OsStr::new("Windows Desktop Guided Tour")
-                .encode_wide()
-                .chain(Some(0).into_iter())
-                .collect::<Vec<_>>()
-                .as_ptr(),
-            0,
-        );
-        return;
-    }
-
-    ShowWindow(handle, SW_SHOW);
-    UpdateWindow(handle);
-
-    // Main message loop:
-    let msg: LPMSG = ptr::null_mut();
-    while GetMessageW(msg, handle, 0, 0) != 0 {
-        TranslateMessage(msg);
-        DispatchMessageW(msg);
-    }
-
-    dbg!((*msg).wParam);
-}
-
-fn main() {
-    #[cfg(windows)]
+fn main() -> Result<(), Box<dyn Error>> {
+    #[cfg(target_os = "windows")]
     unsafe {
         use winapi::um::wincon::{AttachConsole, ATTACH_PARENT_PROCESS};
         AttachConsole(ATTACH_PARENT_PROCESS);
     }
 
-    #[cfg(windows)]
-    unsafe {
-        windows_window()
-    };
+    #[cfg(target_os = "macos")]
+    let child = std::thread::spawn(move || {
+        let app = unsafe { cocoa::appkit::NSApp() };
+        dbg!(1, app);
+        std::thread::sleep(std::time::Duration::from_millis(500));
+        dbg!(2, app);
+    });
+
+    let event_loop = EventLoop::new();
+    let builder = WindowBuilder::new().with_title("test");
+    #[cfg(target_os = "macos")]
+    let builder = builder.with_activation_policy(winit::platform::macos::ActivationPolicy::Regular);
+    let window = builder.build(&event_loop)?;
+
+    #[cfg(target_os = "macos")]
+    {
+        use cocoa::appkit::{
+            NSApp, NSApplication, NSApplicationActivationPolicyRegular, NSEventModifierFlags,
+            NSMenu, NSMenuItem,
+        };
+        use cocoa::base::nil;
+        use cocoa::foundation::NSAutoreleasePool;
+
+        let window_ptr = match window.raw_window_handle() {
+            RawWindowHandle::MacOS(handle) => handle.ns_window,
+            _ => unreachable!(),
+        };
+        dbg!(4, window_ptr);
+
+        // Get a reference to the
+        let app = unsafe { NSApp() };
+        dbg!(3, app);
+        // Don't bother ensuring the activation policy is Regular!
+
+        let pool = unsafe { NSAutoreleasePool::new(nil) };
+
+        // let menubar = NSMenu::new(nil).autorelease();
+
+        // app.setMainMenu_(menubar);
+
+        unsafe { pool.drain() };
+
+        child.join().unwrap();
+    }
+
+    let window = window;
 
     println!("hello world");
 
-    #[cfg(windows)]
-    unsafe {
-        winapi::um::wincon::FreeConsole();
-    }
+    event_loop.run(|event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                println!("The close button was pressed; stopping");
+                *control_flow = ControlFlow::Exit
+            }
+            Event::MainEventsCleared => {
+                // Application update code.
+
+                // Queue a RedrawRequested event.
+                //
+                // You only need to call this if you've determined that you need to redraw, in
+                // applications which do not always need to. Applications that redraw continuously
+                // can just render here instead.
+                // window.request_redraw();
+            }
+            Event::RedrawRequested(_) => {
+                // Redraw the application.
+                //
+                // It's preferable for applications that do not render continuously to render in
+                // this event rather than in MainEventsCleared, since rendering in here allows
+                // the program to gracefully handle redraws requested by the OS.
+            }
+            Event::LoopDestroyed => {
+                #[cfg(target_os = "windows")]
+                unsafe {
+                    winapi::um::wincon::FreeConsole()
+                };
+            }
+            _ => (),
+        }
+    });
 }
