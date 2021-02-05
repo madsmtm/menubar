7704d5fc7642f7f59518c402f587d2d8816370cf
Mads Marquart <mads@marquart.dk>
2021-02-05 10:50:31 +0100

    Change macOS possible API a bit

--- examples/macos_possible_api.rs
+++ examples/macos_possible_api.rs
@@ -1,37 +1,35 @@
-use menubar::{Menu, MenuItem};
+use menubar::{Menu, MenuBar, MenuItem};
 
 fn main() {
     // Dynamic content
     let recent_projects = vec!["a", "a", "a"];
     let recent_files = vec!["x", "y", "zwadjkwahjkdahsbjdlah"];
 
-    // main_menu.set_visible(false);
-    let file_menu = Menu::new("File");
-    file_menu.add(MenuItem::new("New File", "CMD+N", || unimplemented!()));
-    file_menu.add(MenuItem::new("Open...", "CMD+O", || unimplemented!()));
-
-    let open_recent_menu = Menu::new("Open Recent"); // Has dynamic content
-    open_recent_menu.on_open(|menu| {
-        menu.remove_all();
-        menu.add_item(MenuItem::new(
-            "Reopen Closed File",
-            "SHIFT+CMD+T",
-            || unimplemented!(),
-        ));
-        menu.add_separator();
-        recent_projects.iter().for_each(|p| {
-            menu.add_item(MenuItem::new(p, "", || unimplemented!()));
+    let menu_bar = MenuBar::new(|menu| {});
+    menu_bar.add("File", |menu| {
+        menu.add(MenuItem::new("New File", "CMD+N", || unimplemented!()));
+        menu.add(MenuItem::new("Open...", "CMD+O", || unimplemented!()));
+        let open_recent_menu = Menu::new(); // Has dynamic content
+        open_recent_menu.on_open(|menu| {
+            menu.remove_all();
+            menu.add_item(MenuItem::new(
+                "Reopen Closed File",
+                "SHIFT+CMD+T",
+                || unimplemented!(),
+            ));
+            menu.add_separator();
+            recent_projects.iter().for_each(|p| {
+                menu.add_item(MenuItem::new(p, "", || unimplemented!()));
+            });
+            menu.add_separator();
+            recent_files.iter().for_each(|p| {
+                menu.add_item(MenuItem::new(p, "", || unimplemented!()));
+            });
+            menu.add_separator();
+            menu.add_item(MenuItem::new("Clear items", "", || unimplemented!()));
         });
-        menu.add_separator();
-        recent_files.iter().for_each(|p| {
-            menu.add_item(MenuItem::new(p, "", || unimplemented!()));
-        });
-        menu.add_separator();
-        menu.add_item(MenuItem::new("Clear items", "", || unimplemented!()));
+        menu.add_submenu("Open Recent", open_recent_menu);
     });
-    file_menu.add_submenu(open_recent_menu);
-
-    let menu_bar = Menu::new_menu_bar();
-    menu_bar.add_submenu(file_menu);
+    // main_menu.set_visible(false);
     menu_bar.attach_to_application();
 }
